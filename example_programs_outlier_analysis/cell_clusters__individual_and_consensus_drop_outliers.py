2a8a75f92edb89cca81656fc5e06a2fe0c968d33
Victor Klebanoff <67757255+victorkleb@users.noreply.github.com>
2020-09-22 07:26:08 -0400

    added programs for outlier analysis

---
+++ example_programs_outlier_analysis/cell_clusters__individual_and_consensus_drop_outliers.py
@@ -0,0 +1,106 @@
+
+################################################################################################################################
+################################################################################################################################ 
+##                                                                                                                            ##
+##  start program:  cell_clusters__individual_and_consensus_drop_outliers.py                                                  ##                                                                                
+##                                                                                                                            ## 
+################################################################################################################################
+#
+#  invoke FUNCTION  cell_clusters__individual_and_consensus
+#
+################################################################################################################################
+
+import pandas as pd
+import numpy  as np
+
+ 
+
+  
+
+import sys
+sys.path.append("D:/scRNA-seq/Spec_clust_RFproximities_scRNAseq")
+
+
+from  utilities import *
+from FUNCTIONS_Spec_clust_RFproximities_scRNAseq import *
+
+
+ 
+from pathlib import Path
+
+
+
+
+pd.options.display.width = 180
+pd.set_option('display.max_columns', 30)
+ 
+#########################################################################################
+########################################################################################
+
+individual_clusterings_pkl =  "individual_clusterings.pkl"
+consensus_clusterings_pkl =  "consensus_clusterings.pkl"
+consensus_clusterings_H_pkl =  "consensus_clusterings_H.pkl"
+consensus_clusterings_Q_pkl =  "consensus_clusterings_Q.pkl"
+
+
+data_directory  = "Zhengmix8eq"
+data_folder = r"D:/scRNA-seq/" + data_directory
+
+
+cell_data_folder = data_folder + "/cell_data - 5 gene clusters" 
+
+trees_data_folder =  cell_data_folder  +  "/2000 trees drop outliers 8 clusters" 
+trees_data_path = Path ( trees_data_folder )
+
+ 
+
+	
+	
+
+####  log output
+logfile_dsn  =  trees_data_path /  "cell_clusters__individual_and_consensus.txt"
+logfile = open ( logfile_dsn,'w')
+
+# pickle outputs
+individual_clusterings_dsn = trees_data_path / individual_clusterings_pkl
+consensus_clusterings_dsn = trees_data_path / consensus_clusterings_pkl 
+consensus_clusterings_H_dsn = trees_data_path / consensus_clusterings_H_pkl  
+consensus_clusterings_Q_dsn = trees_data_path / consensus_clusterings_Q_pkl  
+
+######################################################################################################################################
+
+result_list =  cell_clusters__individual_and_consensus  ( 20, trees_data_path )
+  
+df_individual_clusterings = result_list[0]
+df_consensus_clusterings = result_list[1]
+df_consensus_clusterings_H = result_list[2]
+df_consensus_clusterings_Q = result_list[3]  
+  
+  
+  
+
+plog ( logfile, '\n\n\n df_individual_clusterings : \n\n', df_individual_clusterings )  
+plog ( logfile, '\n\n\n df_consensus_clusterings_Q: \n\n', df_consensus_clusterings_Q )
+plog ( logfile, '\n\n\n df_consensus_clusterings_H: \n\n', df_consensus_clusterings_H )
+plog ( logfile, '\n\n\n df_consensus_clusterings: \n\n', df_consensus_clusterings ) 
+
+
+df_individual_clusterings.to_pickle ( individual_clusterings_dsn )  
+df_consensus_clusterings_Q.to_pickle ( consensus_clusterings_Q_dsn )
+df_consensus_clusterings_H.to_pickle ( consensus_clusterings_H_dsn )
+df_consensus_clusterings.to_pickle ( consensus_clusterings_dsn )    
+ 
+
+ 
+
+
+logfile.close()
+
+################################################################################################################################ 
+##                                                                                                                            ##
+##  end program:  cell_clusters__individual_and_consensus_drop_outliers.py                                                    ##                                                                              
+##                                                                                                                            ## 
+################################################################################################################################
+################################################################################################################################
+
+
